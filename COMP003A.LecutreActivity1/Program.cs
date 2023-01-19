81c7692e0e2589e99d98537eb9fbfc4009336fdb
williamclements <williamclements@LEM231-32>
2023-01-18 18:56:22 -0800

    Add project files.

---
+++ COMP003A.LecutreActivity1/Program.cs
@@ -0,0 +1,24 @@
+﻿// single-line comment example  
+
+ 
+/* Multi-line
+ * Comment
+ * Example
+ * Author: Anastasia Clements
+ * Course: COMP-003A
+ * Purpose: Lecture Activity for this week
+ * ^Include these three on every assignment^
+ */
+
+namespace COMP003A.LectureActivity1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); // single-line comment example two
+            Console.WriteLine("It's a string!");
+            Console.WriteLine("It's me Ana! ^_^");
+        }
+    }
+}
